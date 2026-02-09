4540085d187b3bb54afac41842735e97b3bb4b6c
abdallahelghobary <abdualahghobary@gmail.com>
2026-02-09 20:36:33 +0200

    Initial Object

--- lib/screens/numbers_page.dart
+++ lib/screens/numbers_page.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class NumbersPage extends StatelessWidget {
   const NumbersPage({super.key});
 
+ final Number One = const Number(
+  image: 'assets/images/numbers/number_one.png', jpName: 'ichi', enName: 'one'
+  );
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,34 +24,50 @@ class NumbersPage extends StatelessWidget {
         color: Color(0xffef9235),
 
         child: Row(
-          children: [Container( 
-           color: Color(0xfffff4d9),
-            child: Image.asset('assets/images/numbers/number_one.png',)),
-          
-          Padding( 
-            padding: const EdgeInsets.only(left: 22),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                
-                Text('ichi',style: TextStyle(color: Colors.white, fontSize: 20),),
-                Text('one',style: TextStyle(color: Colors.white, fontSize: 19), ),
-              ],
+          children: [
+            Container(
+              color: Color(0xfffff4d9),
+              child: Image.asset(One.image),
             ),
-          ),
-             
-            Spacer(flex: 1,),
-                 
-           Padding(
-             padding: const EdgeInsets.only(right: 20),
-             child: Icon(Icons.play_arrow, color: Colors.white, size: 30,
-             
-             ),
-           )
-              
+
+            Padding(
+              padding: const EdgeInsets.only(left: 22),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    One.jpName,
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  Text(
+                    One.enName,
+                    style: TextStyle(color: Colors.white, fontSize: 19),
+                  ),
+                ],
+              ),
+            ),
+
+            Spacer(flex: 1),
+     
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Icon(Icons.play_arrow, color: Colors.white, size: 30),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+class Number {
+final  String image;
+final String jpName;
+final  String enName;
+  
+ const Number({required this.image, required this.jpName, required this.enName});
+
+
+
+
 }
